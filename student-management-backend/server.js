86d9ee18b7f341a90525ea40436fd4fba9d57b44
user1 <manasva541@gmail.com>
2026-06-25 12:54:22 +0530

    allow all origins

--- student-management-backend/server.js
+++ student-management-backend/server.js
@@ -12,13 +12,7 @@ const app = express();
 
 connectDB();
 
-app.use(
-  cors({
-    origin: "https://student-management-system-three-sable.vercel.app",
-    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-    allowedHeaders: ["Content-Type", "Authorization"],
-  })
-);
+app.use(cors());
 app.use((req, res, next) => {
   console.log(req.method, req.url);
   next();
