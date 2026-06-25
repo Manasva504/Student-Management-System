d736bf4f047119dda118d936e98d14f27be4cb2b
user1 <manasva541@gmail.com>
2026-06-25 12:41:16 +0530

    cors modified

--- student-management-backend/server.js
+++ student-management-backend/server.js
@@ -20,6 +20,12 @@ app.use(
   }),
 );
 
+app.options("*", cors());
+
+app.use((req, res, next) => {
+  console.log(req.method, req.url);
+  next();
+});
 app.use(express.json());
 
 app.use("/api/auth", authRoutes);
