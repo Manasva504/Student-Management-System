e84bbe39320c0b2ed5837d402a89d058e71c89f6
user1 <manasva541@gmail.com>
2026-07-01 13:04:49 +0530

    Add SMTP debug logging

--- student-management-backend/utils/sendEmail.js
+++ student-management-backend/utils/sendEmail.js
@@ -1,11 +1,28 @@
 const nodemailer = require("nodemailer");
 
+// const transporter = nodemailer.createTransport({
+//   service: "gmail",
+//   auth: {
+//     user: process.env.EMAIL_USER,
+//     pass: process.env.EMAIL_PASS,
+//   },
+// });
 const transporter = nodemailer.createTransport({
   service: "gmail",
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
   },
+  logger: true,
+  debug: true,
+});
+transporter.verify(function (error, success) {
+  if (error) {
+    console.log("SMTP VERIFY ERROR:");
+    console.log(error);
+  } else {
+    console.log("SMTP Server is ready");
+  }
 });
 
 const sendEmail = async (to, subject, text) => {
